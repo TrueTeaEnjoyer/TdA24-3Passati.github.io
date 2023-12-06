f0e939315f378906a1180cff6b1e6254410dbbdf
Mathe0O0 <91225293+Mathe0O0@users.noreply.github.com>
2023-12-06 02:07:17 +0100

    Add files via upload
    Dělal jsem to přes AI, ale nefuguje mi Docker........... :/, zkusil bys tohle? 
    docker build -t my-hello-world-app .
    docker run -p 8080:8080 my-hello-world-app

---
+++ ProjektTDA/backend/backend/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class ApiController : ControllerBase
+{
+    [HttpGet]
+    [Route("api")]
+    public IActionResult Get()
+    {
+        var secretMessage = new { message = "secret is hard work" };
+        return Ok(secretMessage);
+    }
+}
