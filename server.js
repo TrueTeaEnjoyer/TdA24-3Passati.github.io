144c17b9ae1bf75f9fcf908ba8287815ae313895
Mathe0O0 <91225293+Mathe0O0@users.noreply.github.com>
2024-01-10 22:43:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');  // Přidáno pro manipulaci s cestami
-
+const sqlite3 = require('sqlite3').verbose();
 const port = 8080;
 
 // Nastavení veřejné složky pro statické soubory
 app.use(express.static(path.join(__dirname, 'materials')));
 
-// Nastavení endpointu pro získání hlavní stránky
+// Připojení k db
+const db = new sqlite3.Database('./TdA_DB.db', (err) => {
+    if (err) {
+        console.error('Chyba připojení k databázi:', err.message);
+    } else {
+        console.log('Připojeno k databázi.');    
+    }
+});
+// endpoint hlavní stránky
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-// Použijte /API endpoint pro vracení dat
-app.use('/API', (req, res) => {
-    // Vložte obsah pro /API, pokud je potřeba
 });
 
 app.get('/lecturer', (req, res) => {
     res.sendFile(path.join(__dirname, 'Lecturer.html'));
 });
 
-// Startujte Express server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
