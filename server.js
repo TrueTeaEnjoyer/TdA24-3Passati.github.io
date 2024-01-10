792b8db21b3625ac98e1e25912f3b68ef9d0e263
Mathe0O0 <91225293+Mathe0O0@users.noreply.github.com>
2024-01-10 23:24:30 +0100

    post metoda server.js

--- server.js
+++ server.js
@@ -7,14 +7,16 @@ const port = 8080;
 // Nastavení veřejné složky pro statické soubory
 app.use(express.static(path.join(__dirname, 'materials')));
 
+app.use(express,express.json());
+
 // Nastavení endpointu pro získání hlavní stránky
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Použijte /API endpoint pro vracení dat
-app.use('/API', (req, res) => {
-    // Vložte obsah pro /API, pokud je potřeba
+// endpoint pro přidání učitele
+app.post('/api/lectures', (req, res) => {
+    
 });
 
 app.get('/lecturer', (req, res) => {
