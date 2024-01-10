181adf9f6aefd29bcaddd2996ed191cc4dfdad11
Mathe0O0 <91225293+Mathe0O0@users.noreply.github.com>
2024-01-10 22:48:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');  // Přidáno pro manipulaci s cestami
 const sqlite3 = require('sqlite3').verbose();
 const port = 8080;
 
+// Middleware pro zpracování JSON dat v POST požadavcích
+app.use(bodyParser.json());
 // Nastavení veřejné složky pro statické soubory
 app.use(express.static(path.join(__dirname, 'materials')));
 
@@ -18,6 +20,40 @@ const db = new sqlite3.Database('./TdA_DB.db', (err) => {
 // endpoint hlavní stránky
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+// Endpoint přidat učitele
+app.post('/lecturer', (req, res) => {
+    const newLecturer = req.body;
+
+    // DB dotaz
+    db.run(`INSERT INTO Lectures (uuid, title_before, first_name, middle_name, last_name, title_after, picture_url, location, claim, bio, telephone_numbers, emails, price_per_hour)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+        [
+            newLecturer.uuid,
+            newLecturer.title_before,
+            newLecturer.first_name,
+            newLecturer.middle_name,
+            newLecturer.last_name,
+            newLecturer.title_after,
+            newLecturer.picture_url,
+            newLecturer.location,
+            newLecturer.claim,
+            newLecturer.bio,
+            newLecturer.telephone_numbers,
+            newLecturer.emails,
+            newLecturer.price_per_hour
+        ],
+        function (err) {
+            if (err) {
+                console.error('Chyba při vkládání do databáze:', err.message);
+                res.status(500).json({ error: 'Chyba při vkládání do databáze' });
+            } else {
+                res.json({ message: 'Učitel byl úspěšně přidán do databáze' });
+            }
+        }
+    );
 });
 
 app.get('/lecturer', (req, res) => {
