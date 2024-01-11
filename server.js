9956a3cd5fe42d89656b513e5f69a2d9f2591412
Mathe0O0 <91225293+Mathe0O0@users.noreply.github.com>
2024-01-11 18:00:35 +0100

    Doufám v zázrak s db server.js

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');  // Přidáno pro manipulaci s cestami
 const port = 8080;
-
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 // Nastavení veřejné složky pro statické soubory
 app.use(express.static(path.join(__dirname, 'materials')));
 
+const db = new sqlite3.Database('TdA_DB.db', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the TdA_DB database.');
+});
 // Nastavení endpointu pro získání hlavní stránky
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -19,9 +26,16 @@ app.use('/API', (req, res) => {
 app.get('/lecturer', (req, res) => {
     res.sendFile(path.join(__dirname, 'Lecturer.html'));
 });
-app.get('/lecturers',(req, res) => {
-    
-});   
+app.get('/lecturers', (req, res) => {
+ 
+  db.all('SELECT * FROM lecturers', [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    // Return the lecturers as JSON
+    res.json(rows);
+  });
+});
 // Startujte Express server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
