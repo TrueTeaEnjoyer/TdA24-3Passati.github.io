6f9fd649b5921e9327cd06d3b7a254b0fa8760c9
Mathe0O0 <91225293+Mathe0O0@users.noreply.github.com>
2024-01-11 17:05:21 +0100

    server reset na funkční verzi

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');  // Přidáno pro manipulaci s cestami
-const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
+
 const port = 8080;
-const db = new sqlite3.Database('TdA_DB.db');
-    
 
 // Nastavení veřejné složky pro statické soubory
 app.use(express.static(path.join(__dirname, 'materials')));
 
+// Nastavení endpointu pro získání hlavní stránky
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.use(express,express.json());
-
-
-
-// endpoint pro přidání učitele
-app.post('/api/lectures', (req, res) => {
-    response ("200");
+// Použijte /API endpoint pro vracení dat
+app.use('/API', (req, res) => {
+    // Vložte obsah pro /API, pokud je potřeba
 });
 
 app.get('/lecturer', (req, res) => {
@@ -28,3 +25,4 @@ app.get('/lecturer', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
